1affcecb623ad107b2e4427431e603d2d4c7b0b5
Tuomo Oila <tuomo.oila@gmail.com>
2020-11-30 20:33:19 +0200

    Handle error in the server (and remove next.js's warning about API resolved without sending a response - not really a big deal since it worked, but nicer to keep it clean like this anyway)

--- pages/api/spacex.ts
+++ pages/api/spacex.ts
@@ -1,10 +1,26 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import https from 'https';
+import { resolveSoa } from 'dns';
 
-export default (req: NextApiRequest, res: NextApiResponse): void => {
-  res.statusCode = 200;
-  // TODO: handle error
-  https.get('https://api.spacexdata.com/v4/launches', (response) => {
-    res.json(response);
+export default (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+  return new Promise((resolve) => {
+    try {
+      const request = https.get(
+        'https://api.spacexdata.com/v4/launches',
+        (response) => {
+          res.json(response);
+
+          resolve();
+        },
+      );
+
+      request.end();
+    } catch (error) {
+      // TODO: Ideally we wouldn't use console
+      console.error(error);
+      res.status(500).end();
+
+      return resolve();
+    }
   });
 };
